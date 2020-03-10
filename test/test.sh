f9a5abe685158d09e96205d6f38a12661b6349cb
Steve Milner <smilner@redhat.com>
2020-03-10 14:32:24 -0400

    Add validation test via ignition-validate
    Signed-off-by: Steve Milner <smilner@redhat.com>

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# If IGNITION_VALIDATE is not set to a specific binary
+# check to see if we have it locally
+if [[ -z ${IGNITION_VALIDATE} ]]; then
+    # Check local directory
+    ls ignition-validate 2>&1 /dev/null
+    if [[ $? == 0 ]]; then
+        IGNITION_VALIDATE="$(pwd)/ignition-validate"
+    else
+        echo "- ignition-validate not found. Skipping validation."
+    fi
+fi
+
+if [[ ${IGNITION_VALIDATE} != "" ]]; then
+    echo "+ Found ignition-validate at ${IGNITION_VALIDATE}"
+    echo "+ $(${IGNITION_VALIDATE} --version)"
+fi
+
 NEWFILES='"/etc/hostname"
 "/etc/resolve.conf"
 "/etc/sysconfig/network-scripts/ifcfg-fake"
@@ -18,6 +35,22 @@ for required_command in jq file; do
         exit 1
     fi
 done
+
+# validate the output (if ignition-validate is available)
+validate_ignition() {
+    tmpfile=$1
+    testname=$2
+    if [[ ${IGNITION_VALIDATE} != "" ]]; then
+        ${IGNITION_VALIDATE} $tmpfile
+        if [[ $? == 0 ]]; then
+            echo "PASS: Validate config for ${testname}"
+        else
+            echo "FAIL: Validation failed for ${testname}"
+        fi
+    else
+        echo "SKIP: Can not validate file for ${testname}"
+    fi
+}
 
 # Ensure the new files from the fakeroot are present
 test_expected_files() {
@@ -97,18 +130,21 @@ test_expected_contents() {
 test_name="Ignition With No Storage"
 tmpfile=$(mktemp)
 ./filetranspile -i test/ignition-no-storage.json -f test/fakeroot > ${tmpfile}
+validate_ignition "${tmpfile}" "${test_name}"
 test_expected_files "${tmpfile}" "${test_name}"
 test_expected_keys "${tmpfile}" "${test_name}"
 
 test_name="Ignition With No Files"
 tmpfile=$(mktemp)
 ./filetranspile -i test/ignition-no-files.json -f test/fakeroot > ${tmpfile}
+validate_ignition "${tmpfile}" "${test_name}"
 test_expected_files "${tmpfile}" "${test_name}"
 test_expected_keys "${tmpfile}" "${test_name}"
 
 test_name="Ignition With Existing Files"
 tmpfile=$(mktemp)
 ./filetranspile -i test/ignition.json -f test/fakeroot > ${tmpfile}
+validate_ignition "${tmpfile}" "${test_name}"
 test_expected_files "${tmpfile}" "${test_name}"
 test_expected_keys "${tmpfile}" "${test_name}"
 
@@ -116,6 +152,7 @@ test_expected_keys "${tmpfile}" "${test_name}"
 test_name="Ignition Overwrite With Fakeroot File"
 tmpfile=$(mktemp)
 ./filetranspile -i test/ignition-overwrite-with-fakeroot-file.json -f test/fakeroot > ${tmpfile}
+validate_ignition "${tmpfile}" "${test_name}"
 test_expected_files "${tmpfile}" "${test_name}"
 test_expected_keys "${tmpfile}" "${test_name}"
 # We should still have the fakeroot one and not the one in our original ignition
